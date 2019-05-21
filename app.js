80ca17ff7fc6a37400506c996e4b11bc97f8b606
seebrucke <mojtaba.zargar2010@gmail.com>
2019-05-21 19:18:49 +0200

    third

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '/src/views'));
 const rout = require('./src/routes/mainRoutes');
 const port = process.env.PORT || 3000;
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/',rout);
 app.listen(port,()=>{
